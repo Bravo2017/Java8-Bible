d09fbc644fbab9335c5a241a6f6c47a4647d621a
mukadder <hht179@gmail.com>
2016-10-01 12:48:38 -0400

    vdsdas

--- src/test/java/bbejeck/collector/LambdaTest.java
+++ src/test/java/bbejeck/collector/LambdaTest.java
@@ -3,15 +3,25 @@ package bbejeck.collector;
 import com.google.common.collect.Lists;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import static java.util.Collections.emptySet;
 import java.util.Comparator;
+import java.util.HashMap;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
+import java.util.stream.Collector;
+import java.util.stream.Collector.Characteristics;
 import java.util.stream.Collectors;
 import java.util.stream.DoubleStream;
 import java.util.stream.Stream;
@@ -62,10 +72,61 @@ public class LambdaTest {
         Stream<Integer>stream =Stream.of(1,2,3,4);
         assertThat(stream.max(Comparator.naturalOrder()).get(),is(4));
     }
+    @Test 
+    
+    public void testCountCapitalLetters(){
+        String sentence = "I love Sunny Leonne";
+        int result =0;
+        for(char c:sentence.toCharArray()){
+            if(c>='A'&&c<='Z'){
+                result++;
+            }
+        }
+        assertThat(result,is(3));
+    }
+    
+    @Test 
+    
+    public void weLovesunny_in_lamda(){
+        String sentence = "I love Sunny Leonne";
+        long count 
+        =sentence.codePoints().filter(c->c>='A'&& c<='Z').count();
+        assertThat(count,is(3L));
+    }
+    @Test
+    
+    public void testCollector(){
+        // a,a,a,b,b,c
+        // Optional<a> 
+        //a:3,b:2,c;1 Map<X,Integer>
+        // Collector<X,Map<X,Integer>,Optial<X>
+        Optional<Integer> x = Lists.newArrayList(1,1,2,2,2,2,3,3,5,5)
+                .stream().collect(new MostPopular<>());
+        assertThat(x.get(),is(2));
+        Map<Integer,String> map = new HashMap<>();
+       map.put(1, "A");
+       map.put(2, "B");
+       map.put(3, "C");
+       BiConsumer<Integer,String> biConsumer = (key,value) -> 
+						System.out.println("Key:"+ key+" Value:"+ value);
+       map.forEach(biConsumer);
+    }
+    @Test 
+    public void lengghtChecker(){
+        
+         final Function<Integer, Predicate<String>> lengthChecker = (Integer length) -> {
+            Predicate<String> lengthCheck = str -> str.length() > length;
+            return lengthCheck;
+      };
+         String str= "padu";
+         assertThat(lengthChecker.apply(3).test(str),is(false));
+         
+    }
     
     @Test
     public void newMathTest_for_Optional() {
         double[] doubles ={1.0,2.0,-1.0,0.0};
+        
         List<Double>result= new ArrayList<>();
         DoubleStream.of(doubles).forEach(d->NewMath.sqrt(d).ifPresent(result::add));
         assertThat(result.size(),is(2));
@@ -84,5 +145,58 @@ public class LambdaTest {
             return d!=0 ? Optional.of(1/d):Optional.empty();
         }
     }
+    interface Collecter<T,A,R>{
+        Supplier<A> supplier();
+        
+        BiConsumer<A,T> accumulator();
+        BinaryOperator<A>combinor();
+        Function<A,R> finsher();
+        Set<Characteristics> characteristics();
+        
+        
+        
+        
+        
+    
+}
+    public class MostPopular<T> implements Collector<T,Map<T,Integer>,Optional<T>>{
+
+        @Override
+        public Supplier<Map<T, Integer>> supplier() {
+          //  throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return HashMap::new;
+        }
+        
+
+        @Override
+        public BiConsumer<Map<T, Integer>, T> accumulator() {
+            //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return (acc,elem)->acc.merge(elem, 1, (old,v)->old+v);
+        }
+
+        @Override
+        public BinaryOperator<Map<T, Integer>> combiner() {
+            //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return (acc1,acc2) ->{
+            throw new UnsupportedOperationException();
+        };
+        }
+
+        @Override
+        public Function<Map<T, Integer>, Optional<T>> finisher() {
+            //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return (acc)->acc.entrySet().stream()
+                .reduce((a,b)->a.getValue()>b.getValue() ?a:b)
+                .map(Map.Entry::getKey);
+        }
+
+        @Override
+        public Set<Characteristics> characteristics() {
+            //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return Collections.emptySet();
+        }
+        
+    }
+    
     
 }
